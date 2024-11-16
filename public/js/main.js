a789b9770d74abd74e9a2cd22042c49723c56d95
Rohit Kashyap <rohitdevofficial@gmail.com>
2024-11-16 21:28:35 +0530

    16 November

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('JavaScript file is working!');
   });
+$(".dropdown").mouseenter(function(){
+  $(this).parentsUntil("body").addClass("active")
+})
+$(".dropdown").mouseleave(function(){
+  $(this).parentsUntil("body").removeClass("active")
+})
+$(".indus h4").hover(function(){
+  let type = $(this).attr("data-id")
+  $(`.drop-images`).removeClass('active')
+  $(`[data-type='${type}']`).addClass('active')
+})
